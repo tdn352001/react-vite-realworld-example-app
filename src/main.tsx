7b784b1072ba2e9c12cea430d07332138a9cf5b0
tdn352001 <tdn352001@gmail.com>
2025-02-17 10:40:04 +0700

    UPdate 123

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { configureAxios } from 'api-services'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+
+configureAxios({
+  baseURL: 'https://api.optimai.network',
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
